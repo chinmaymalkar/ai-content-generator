8738780d60e70c89c83f20321383e9f279fd1c07
chinmaymalkar <124694179+chinmaymalkar@users.noreply.github.com>
2024-07-16 22:22:08 +0530

    Update Usestrack.tsx

--- app/dashboard/_components/Usestrack.tsx
+++ app/dashboard/_components/Usestrack.tsx
@@ -1,67 +1,56 @@
-'use client'
+'use client';
 import { aioutput } from '@/utils/schema';
-import { useUser } from '@clerk/nextjs'
-import React, { useEffect, useState } from 'react'
-import {eq} from 'drizzle-orm'
+import { useUser } from '@clerk/nextjs';
+import React, { useEffect, useState } from 'react';
+import { eq } from 'drizzle-orm';
 import { db } from '@/utils/db';
 
-
-export interface HISTORY{
-  id:Number,
-  formData:string,
-  aiResponse:string,
-  templateSlug:string,
-  createdBy:string,
-  createdAt:string
-}
-
-
 function Usestrack() {
+  const user = useUser();
+  const email: string | undefined = user?.user?.emailAddresses?.[0]?.emailAddress;
+  const [totalUses, setTotalUses] = useState<number>(0);
 
-    const user =useUser();
-    const email = user?.user?.emailAddresses?.[0]?.emailAddress;
-    const [totalUses,setTotalUses]=useState();
-    useEffect(()=>{
-        user && getdata()
-    },[user])
-
-    const getdata= async()=>{
-      // const result = await db.select()
-      //   .from(aioutput)
-      //   .where(eq(aioutput.createdBy, user?.user?.emailAddresses[0].emailAddress));  
-        // const result :string = await db.select()
-        // .from(aioutput)
-        // .where(eq(aioutput.createdBy, email));
-      const result : any=await db.select().from(aioutput).where(eq(aioutput.createdBy,user?.user?.emailAddresses[0].emailAddress))
-        gettotaluses(result);
-
+  useEffect(() => {
+    if (email) {
+      getdata();
     }
-    const gettotaluses=(result)=>{
-        let total:number=0;
-        result.forEach(element => {
-            total=total+Number(element.aiResponse?.length)
-            
-        });
-        setTotalUses(total)
-        console.log(total)
+  }, [email]);
+
+  const getdata = async () => {
+    if (email) {
+      const result = await db
+        .select()
+        .from(aioutput)
+        .where(eq(aioutput.createdBy, email));
+      gettotaluses(result);
     }
+  };
+
+  const gettotaluses = (result: any) => {
+    let total: number = 0;
+    result.forEach((element: any) => {
+      total = total + Number(element.aiResponse?.length);
+    });
+    setTotalUses(total);
+    console.log(total);
+  };
 
   return (
     <div className='m-5'>
       <div className='bg-primary text-white p-3 rounded-lg'>
         <h2 className='font-medium p-2'>Credits</h2>
         <div className='h-2 bg-[#9981f9] rounded-full w-full '>
-            <div className='h-2 bg-white rounded-full ' style={{
-                width: ((totalUses%10000)/10000)*100+'%'
-            }}>
-
-            </div>
+          <div
+            className='h-2 bg-white rounded-full '
+            style={{
+              width: ((totalUses % 10000) / 10000) * 100 + '%',
+            }}
+          ></div>
         </div>
-        <h2 className='text-sm py-2'>{totalUses %10000}/10000 credits used</h2>
+        <h2 className='text-sm py-2'>{totalUses % 10000}/10000 credits used</h2>
       </div>
-      
     </div>
-  )
+  );
 }
 
-export default Usestrack
+export default Usestrack;
